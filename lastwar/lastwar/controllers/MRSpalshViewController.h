da52724d7ae06f6d6d7640902973a130515506e8
Michail Zinov <zinov.m.v@gmail.com>
2015-04-24 22:38:21 +1000

    Пролеиаем сплеш экран

---
+++ lastwar/lastwar/controllers/MRSpalshViewController.h
@@ -0,0 +1,18 @@
+//
+//  MRSpalshViewController.h
+//  lastwar
+//
+//  Created by Зинов Михаил  on 24.04.15.
+//  Copyright (c) 2015 MAYAK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MRSpalshViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIView *topView;
+@property (strong, nonatomic) IBOutlet UIView *bottonView;
+@property (strong, nonatomic) IBOutlet UIImageView *shieldImageVIew;
+@property (strong, nonatomic) IBOutlet UIImageView *weaponsImageView;
+
+@end
---
+++ lastwar/lastwar/controllers/MRSpalshViewController.m
@@ -0,0 +1,45 @@
+//
+//  MRSpalshViewController.m
+//  lastwar
+//
+//  Created by Зинов Михаил  on 24.04.15.
+//  Copyright (c) 2015 MAYAK. All rights reserved.
+//
+
+#import "MRSpalshViewController.h"
+#import "MRMenuViewController.h"
+
+@interface MRSpalshViewController ()
+{
+    NSTimer *timer;
+}
+
+
+@end
+
+@implementation MRSpalshViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [super viewDidLoad];
+    timer = [NSTimer scheduledTimerWithTimeInterval:2.0f  target:self selector:@selector(showMainMenuController) userInfo:nil repeats:YES];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)showMainMenuController
+{
+    MRMenuViewController* menuController = [[MRMenuViewController alloc] init];
+    [self presentViewController:menuController animated:YES completion:^{
+        ;
+    }];
+    
+}
+
+
+
+@end
