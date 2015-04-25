1ef644b644309fc5be041f59c82095c03d63ccd6
Michail Zinov <zinov.m.v@gmail.com>
2015-04-25 12:32:21 +1000

    Добавляю стрельбу

---
+++ lastwar/lastwar/objects/MRBulletNode.h
@@ -0,0 +1,13 @@
+//
+//  MRBulletNode.h
+//  lastwar
+//
+//  Created by Зинов Михаил  on 25.04.15.
+//  Copyright (c) 2015 MAYAK. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MRBulletNode : SKSpriteNode
+
+@end
---
+++ lastwar/lastwar/objects/MRBulletNode.m
@@ -0,0 +1,13 @@
+//
+//  MRBulletNode.m
+//  lastwar
+//
+//  Created by Зинов Михаил  on 25.04.15.
+//  Copyright (c) 2015 MAYAK. All rights reserved.
+//
+
+#import "MRBulletNode.h"
+
+@implementation MRBulletNode
+
+@end
